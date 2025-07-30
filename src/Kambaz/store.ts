609fb156838818fad6d89d782af9b7b187ff8b4f
ninaghar23 <gharachorloo.ni@northeastern.edu>
2025-07-29 20:55:19 -0400

    5th commit

--- src/Kambaz/store.ts
+++ src/Kambaz/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modulesReducer from "./Courses/Modules/reducer";
 import accountReducer from "./Account/reducer";
-import { assignments } from "./Database";
+
 import coursesReducer from "./Courses/reducer";
 // Update the path below if 'Assignments/reducer' is actually under './Courses/Assignments/reducer'
 import assignmentsReducer from "./Courses/Assignments/reducer";
