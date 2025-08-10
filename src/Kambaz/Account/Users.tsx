24d509ee9961c60bd740b100fd0dba4d8c3d5171
ninaghar23 <gharachorloo.ni@northeastern.edu>
2025-08-10 19:55:10 -0400

    3rd commit

--- src/Kambaz/Account/Users.tsx
+++ src/Kambaz/Account/Users.tsx
@@ -8,7 +8,7 @@ export default function Users() {
  const [users, setUsers] = useState<any[]>([]);
  const [role, setRole] = useState("");
  // eslint-disable-next-line @typescript-eslint/no-unused-vars
- const [name, setName] = useState("");
+ const [, setName] = useState("");
  const createUser = async () => {
     try {
         console.log("Creating user...");
